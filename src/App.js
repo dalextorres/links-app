08743f68d8d527c45d53671e2a38766224af8248
Alex Torres <dalextor@gmail.com>
2021-03-23 19:00:19 -0600

    Updates

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import Button from "./components/elements/Button";
 import IG from "./assets/instagram-logo.svg"
 import FB from "./assets/facebook-logo.svg"
 import WP from "./assets/whatsapp-logo.svg"
+import G from "./assets/globe.svg"
 
 function App() {
   return (
     <div className="main-wrapper">
           <h1 className="main-title">Just for Kicks</h1>
-          <Button icon={IG} link="https://google.com"/>
-          <Button icon={FB} link="https://google.com"/>
-          <Button icon={IG} link="https://google.com"/>
-          <Button icon={WP} link="https://google.com"/>
+          <Button icon={G} link="https://google.com" description="Haz tu compra en nuestra página web"/>
+          <Button icon={FB} link="https://google.com" description="Síguenos en Facebook"/>
+          <Button icon={IG} link="https://google.com" description="Síguenos en Instagram"/>
+          <Button icon={WP} link="https://google.com" description="81 1212 3390"/>
     </div>
   );
 }
--- src/components/elements/Button.jsx
+++ src/components/elements/Button.jsx
@@ -3,8 +3,9 @@ export default function Button(props) {
         <a href={props.link} className="link">
             <div className="button-wrapper">
                 <div>
-                    <img src={props.icon} style={{height: 70}}/>
+                    <img src={props.icon} style={{height: 45}}/>
                 </div>
+                <p className="button-title">{props.description}</p>
             </div>
         </a>
     )
