ec355abb10ecca9f95f6fa1953d5af9c62cb312b
Alex Torres <dalextor@gmail.com>
2021-03-25 16:34:51 -0600

    Add logo

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import IG from "./assets/instagram-logo.svg"
 import FB from "./assets/facebook-logo.svg"
 import WP from "./assets/whatsapp-logo.svg"
 import G from "./assets/globe.svg"
+import SC from "./assets/streetclub.png"
 
 function App() {
   return (
     <div className="main-wrapper">
-          <h1 className="main-title">Just for Kicks</h1>
+          <img src={SC} style={{height:280}} className="logo"/>
           <Button icon={G} link="https://google.com" description="Haz tu compra en nuestra página web"/>
           <Button icon={FB} link="https://google.com" description="Síguenos en Facebook"/>
           <Button icon={IG} link="https://google.com" description="Síguenos en Instagram"/>
