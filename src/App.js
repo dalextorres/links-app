9818064c5b918fb80a7fd80dfa6ba5b4cdd6cbd0
Alex Torres <dalextor@gmail.com>
2021-03-25 17:44:01 -0600

    Update page

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import IG from "./assets/instagram-logo.svg"
 import FB from "./assets/facebook-logo.svg"
 import WP from "./assets/whatsapp-logo.svg"
 import G from "./assets/globe.svg"
-import SC from "./assets/streetclub.png"
+import SC from "./assets/streetclub.svg"
 
 function App() {
   return (
     <div className="main-wrapper">
-          <img src={SC} style={{height:280}} className="logo"/>
-          <Button icon={G} link="https://google.com" description="Haz tu compra en nuestra página web"/>
+          <img src={SC} style={{height:180}} className="logo"/>
+          <Button id="black-background" icon={G} link="https://google.com" description="Haz tu compra en nuestra página web"/>
           <Button icon={FB} link="https://google.com" description="Síguenos en Facebook"/>
-          <Button icon={IG} link="https://google.com" description="Síguenos en Instagram"/>
+          <Button id="black-background" icon={IG} link="https://google.com" description="Síguenos en Instagram"/>
           <Button icon={WP} link="https://google.com" description="81 1212 3390"/>
     </div>
   );
--- src/components/elements/Button.jsx
+++ src/components/elements/Button.jsx
@@ -1,7 +1,7 @@
 export default function Button(props) {
     return (
         <a href={props.link} className="link">
-            <div className="button-wrapper">
+            <div className="button-wrapper" id={props.id}>
                 <div>
                     <img src={props.icon} style={{height: 45}}/>
                 </div>
