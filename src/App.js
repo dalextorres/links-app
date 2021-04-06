c1d4d65209f6a0fc0e77b853712b7901870974f4
Alex Torres <dalextor@gmail.com>
2021-04-05 22:30:33 -0500

    Add social media links

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import FB from "./assets/facebook-logo.svg"
 import WP from "./assets/whatsapp-logo.svg"
 import G from "./assets/globe.svg"
 import SC from "./assets/streetclub-white.svg"
+import TT from "./assets/tik-tok.svg"
 
 function App() {
   return (
     <div className="main-wrapper">
           <img src={SC} style={{height:180}} className="logo"/>
           <div className="buttons-wrapper">
-            <Button id="black-background" icon={G} link="https://google.com" />
-            <Button icon={FB} link="https://google.com" description="Síguenos en Facebook"/>
-            <Button id="black-background" icon={IG} link="https://google.com"/>
-            <Button icon={WP} link="https://wa.me/5218119106600" />
+            <Button id="black-background" icon={G} link="https://streetclub.mx" />
+            <Button icon={FB} link="https://www.facebook.com/St.Clubmx" description="Síguenos en Facebook"/>
+            <Button id="black-background" icon={IG} link="https://www.instagram.com/streetclub_mx/?hl=es-la"/>
+            <Button icon={WP} link="https://wa.me/message/IIPNMRKJDH6JF1" />
+            <Button icon={TT} link = "https://vm.tiktok.com/ZMefeYTp2" id="tiktok-logo"/>
           </div>
     </div>
   );
