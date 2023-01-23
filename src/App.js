6d98db847ec85388a6dc4f730fbe5d69e37ee647
Alex Torres <dalextor@gmail.com>
2023-01-23 16:59:13 -0600

    add tiktok link

--- src/App.js
+++ src/App.js
@@ -11,13 +11,13 @@ import StreeClub from "./assets/street_club_mx_logo.jpg"
 function App() {
   return (
     <div className="main-wrapper">
-          <img src={StreeClub} style={{height:180}} className="logo"/>
+          <img src={StreeClub} style={{height:230}} className="logo"/>
           <div className="buttons-wrapper">
             <Button id="black-background" icon={G} link="https://streetclub.mx" />
             <Button icon={FB} link="https://www.facebook.com/St.Clubmx" description="Síguenos en Facebook"/>
             <Button id="black-background" icon={IG} link="https://www.instagram.com/streetclub_mx/?hl=es-la"/>
             <Button icon={WP} link="https://wa.me/message/24DVKGCIDNCFI1" />
-            <Button icon={TT} link = "https://vm.tiktok.com/ZMefeYTp2" id="tiktok-logo"/>
+            <Button icon={TT} link = "https://www.tiktok.com/@streetclub_mx?t=8Z8W7Q0wUab&_r=1" id="tiktok-logo"/>
           </div>
     </div>
   );
